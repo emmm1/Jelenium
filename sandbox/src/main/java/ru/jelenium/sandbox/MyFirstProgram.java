26f8b4155cc7cec67cf6af95532c6df2fc16c76a
emmm1 <emmm2008@ya.ru>
2016-02-24 16:54:48 +0300

    Создал пакет и перенес туда свои классы

--- sandbox/src/main/java/ru/jelenium/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/jelenium/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
+package ru.jelenium.sandbox;
+
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+				System.out.println("Hello, world!");
 	}
 }
---
+++ sandbox/src/main/java/ru/jelenium/sandbox/Point.java
@@ -0,0 +1,7 @@
+package ru.jelenium.sandbox;
+
+/**
+ * Created by mikhail.evseev on 24.02.2016.
+ */
+public class Point {
+}

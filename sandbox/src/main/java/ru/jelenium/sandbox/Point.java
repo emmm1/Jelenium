3353ec31f10b81096c59a87e9c5763fcefb3bbd6
emmm1 <emmm2008@ya.ru>
2016-02-28 18:53:30 +0300

    Сделал приватным метод возведения в квадрат катета в классе Point

--- sandbox/src/main/java/ru/jelenium/sandbox/Point.java
+++ sandbox/src/main/java/ru/jelenium/sandbox/Point.java
@@ -18,11 +18,11 @@ public class Point {
 
   // общая переменная для возведения в квадрат
   public double distance(Point p2) {
-    double dist = sqrt(Point.sqdiff(this.x, p2.x) + Point.sqdiff(this.y, p2.y));
+    double dist = sqrt(sqdiff(this.x, p2.x) + sqdiff(this.y, p2.y));
     return dist;
   }
 
-  public static double sqdiff(double p1, double p2) {
+  static double sqdiff(double p1, double p2) {
     // Возвращает квадрат разницы по одной оси
     double sqd = pow((p1 - p2), Point.mpower);
     return sqd;

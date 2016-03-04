70d0767c97cfd167add1f20168e3781eec3cc245
emmm1 <emmm2008@ya.ru>
2016-03-04 20:07:13 +0300

    задание 5 промежуточный комит

--- addressbook-web-tests/src/test/java/ru/jelenium/addressbook/appmanager/AppMangerSIDE.java
+++ addressbook-web-tests/src/test/java/ru/jelenium/addressbook/appmanager/AppMangerSIDE.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
-import ru.jelenium.addressbook.model.ContactData;
 import ru.jelenium.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
@@ -17,8 +15,8 @@ import static org.testng.Assert.fail;
  * Created by mikhail.evseev on 04.03.2016.
  */
 public class AppMangerSIDE {
-  //private WebDriver driver;
-  public FirefoxDriver driver;
+  FirefoxDriver driver;
+  private ContactHelperSIDE contactHelperSIDE;
   public String baseUrl;
   public boolean acceptNextAlert = true;
   public StringBuffer verificationErrors = new StringBuffer();
@@ -27,6 +25,7 @@ public class AppMangerSIDE {
     driver = new FirefoxDriver();
     baseUrl = "http://localhost/addressbook/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    contactHelperSIDE = new ContactHelperSIDE(driver);
     login("admin", "secret");
   }
 
@@ -37,56 +36,6 @@ public class AppMangerSIDE {
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys(passwd);
     driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
-  }
-
-  public void fillOutForm(ContactData contactData) {
-    driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-    driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
-    driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-    driver.findElement(By.name("nickname")).clear();
-    driver.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
-    driver.findElement(By.name("title")).clear();
-    driver.findElement(By.name("title")).sendKeys(contactData.getTitle());
-    driver.findElement(By.name("company")).clear();
-    driver.findElement(By.name("company")).sendKeys(contactData.getTextInfo().getCompany());
-    driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys(contactData.getTextInfo().getAddress1());
-    driver.findElement(By.name("home")).clear();
-    driver.findElement(By.name("home")).sendKeys(contactData.getPhone().getHome1());
-    driver.findElement(By.name("mobile")).clear();
-    driver.findElement(By.name("mobile")).sendKeys(contactData.getPhone().getMobile());
-    driver.findElement(By.name("work")).clear();
-    driver.findElement(By.name("work")).sendKeys(contactData.getPhone().getWork());
-    driver.findElement(By.name("fax")).clear();
-    driver.findElement(By.name("fax")).sendKeys(contactData.getPhone().getFax());
-    driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys(contactData.getContactEData().getEmail1());
-    driver.findElement(By.name("email2")).clear();
-    driver.findElement(By.name("email2")).sendKeys(contactData.getContactEData().getEmail2());
-    driver.findElement(By.name("email3")).clear();
-    driver.findElement(By.name("email3")).sendKeys(contactData.getContactEData().getEmail3());
-    driver.findElement(By.name("homepage")).clear();
-    driver.findElement(By.name("homepage")).sendKeys(contactData.getContactEData().getHomepage());
-    new Select(driver.findElement(By.name("bday"))).selectByVisibleText("13");
-    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText("October");
-    driver.findElement(By.name("byear")).clear();
-    driver.findElement(By.name("byear")).sendKeys("1984");
-    new Select(driver.findElement(By.name("aday"))).selectByVisibleText("27");
-    new Select(driver.findElement(By.name("amonth"))).selectByVisibleText("October");
-    driver.findElement(By.name("ayear")).clear();
-    driver.findElement(By.name("ayear")).sendKeys("2001");
-    new Select(driver.findElement(By.name("new_group"))).selectByVisibleText("Test group for Selenium Builder");
-    driver.findElement(By.name("address2")).clear();
-    driver.findElement(By.name("address2")).sendKeys(contactData.getTextInfo().getAddress2());
-    driver.findElement(By.name("phone2")).clear();
-    driver.findElement(By.name("phone2")).sendKeys(contactData.getPhone().getHome2());
-    driver.findElement(By.name("notes")).clear();
-    driver.findElement(By.name("notes")).sendKeys(contactData.getTextInfo().getNotes());
-    // ERROR: Caught exception [Error: Dom locators are not implemented yet!]
-    driver.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
   public void gotoAddNewPage() {
@@ -150,5 +99,9 @@ public class AppMangerSIDE {
 
   public void gotoGroupPage() {
     driver.findElement(By.linkText("groups")).click();
+  }
+
+  public ContactHelperSIDE getContactHelperSIDE() {
+    return contactHelperSIDE;
   }
 }
--- addressbook-web-tests/src/test/java/ru/jelenium/addressbook/tests/NewRecordSIDEtest.java
+++ addressbook-web-tests/src/test/java/ru/jelenium/addressbook/tests/NewRecordSIDEtest.java
@@ -11,7 +11,7 @@ public class NewRecordSIDEtest extends SIDEBase {
   @Test
   public void testNewRecordSIDE() throws Exception {
     app.gotoAddNewPage();
-    app.fillOutForm(
+    app.getContactHelperSIDE().fillOutForm(
             new ContactData("dfsdafsdf", "dsfasdf", "asdfsdf", "dfdf", "dfasdf", new ContactTextInfo("fasd dsfdff", "fzsdfasxdfsf sdf zfzxdffd  dzfzxd",
                     "sdfsd,fkjlkjdsfjdfsl;jf ;ljsdf;lj sadl;fj l;kjla; fdsjlsa;dfj", "dsfsdafasdfk;l;<f;alks fd'k;lasdf;l,ksdaf;lk SIDE"),
                     new ContactPhone("6464165", "41616468721", "4654463416514", "41646161", "65468515451"),
